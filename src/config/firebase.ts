007f79bfd8a007fa741be56002ec0e4e79c0a4c4
baagenciaweb-sys <ba.agenciaweb@gmail.com>
2026-05-30 17:27:10 -0300

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -4,16 +4,15 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || AIzaSyDpAe1lls4RZcWXvjl2eVrbifbYcLJb4GE
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || ba-agencia-web-plantillas-bolt.firebaseapp.com
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || ba-agencia-web-plantillas-bolt
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || ba-agencia-web-plantillas-bolt.firebasestorage.app,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || 721875489030,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || 1:721875489030:web:911060fdff01c642b74e87
+  apiKey: "AIzaSyDpAe1lls4RZcWXvjl2eVrbifbYcLJb4GE",
+  authDomain: "ba-agencia-web-plantillas-bolt.firebaseapp.com",
+  projectId: "ba-agencia-web-plantillas-bolt",
+  storageBucket: "ba-agencia-web-plantillas-bolt.firebasestorage.app",
+  messagingSenderId: "721875489030",
+  appId: "1:721875489030:web:911060fdff01c642b74e87"
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
